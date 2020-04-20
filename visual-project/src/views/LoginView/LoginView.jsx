1916aced6b9edfea55109b7995ddda6750c9153e
sinx-wang <cosx1999@hotmail.com>
2020-04-20 17:31:12 +0800

    更改页面名称

--- visual-project/src/views/LoginView/LoginView.jsx
+++ visual-project/src/views/LoginView/LoginView.jsx
@@ -75,6 +75,11 @@ const useStyles = makeStyles((theme) => ({
 export default function LoginView() {
   const classes = useStyles();
 
+  // React Hooks，详见https://zh-hans.reactjs.org/docs/hooks-effect.html
+  React.useEffect(() => {
+    document.title = "登录";
+  });
+
   // React Hooks，相当于class式写法的state，详见https://zh-hans.reactjs.org/docs/hooks-intro.html
   const [values, setValues] = React.useState({
     username: "",
