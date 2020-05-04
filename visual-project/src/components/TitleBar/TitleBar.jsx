f0f0845c5643efee2f805f5d0f0dcf55a4d81529
sinx-wang <cosx1999@hotmail.com>
2020-05-04 19:49:33 +0800

    添加头像

--- visual-project/src/components/TitleBar/TitleBar.jsx
+++ visual-project/src/components/TitleBar/TitleBar.jsx
@@ -2,52 +2,61 @@ import React from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import AppBar from "@material-ui/core/AppBar";
-import Toobar from "@material-ui/core/Toolbar";
+import Avatar from "@material-ui/core/Avatar";
+import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Badge from "@material-ui/core/Badge";
 import NotificationIcon from "@material-ui/icons/Notifications";
 import MenuIcon from "@material-ui/icons/Menu";
+import Person from "@material-ui/icons/Person";
+import ExitToApp from "@material-ui/icons/ExitToApp";
+import { pink } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
   toolBar: {
-    paddingRight: 24
+    paddingRight: 24,
   },
   toolbarIcon: {
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-end",
     padding: "0 8px",
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: 36
+    marginRight: 36,
   },
   menuButtonHidden: {
-    display: "none"
+    display: "none",
   },
-  appBarSpacer: theme.mixins.toolbar
+  appBarSpacer: theme.mixins.toolbar,
+  pink: {
+    marginRight: 10,
+    color: theme.palette.getContrastText(pink[500]),
+    backgroundColor: pink[500],
+  },
 }));
 
 export default function TitleBar(props) {
@@ -63,7 +72,7 @@ export default function TitleBar(props) {
       position="absolute"
       className={clsx(classes.appBar, props.open && classes.appBarShift)}
     >
-      <Toobar className={classes.toolBar}>
+      <Toolbar className={classes.toolBar}>
         <IconButton
           edge="start"
           color="inherit"
@@ -84,12 +93,18 @@ export default function TitleBar(props) {
         >
           {props.content}
         </Typography>
+        <Avatar className={classes.pink}>
+          <Person />
+        </Avatar>
         <IconButton color="inherit">
           <Badge badgeContent={4} color="secondary">
             <NotificationIcon />
           </Badge>
         </IconButton>
-      </Toobar>
+        <IconButton color="inherit">
+          <ExitToApp />
+        </IconButton>
+      </Toolbar>
     </AppBar>
   );
 }
@@ -97,5 +112,5 @@ export default function TitleBar(props) {
 TitleBar.propTypes = {
   open: PropTypes.bool,
   content: PropTypes.string,
-  handleClickMenuButton: PropTypes.func
+  handleClickMenuButton: PropTypes.func,
 };
