e26c14dd8f268f4a3f8b5e6520bde7f450c86c09
xihalameilin <1293086146@qq.com>
2020-05-15 21:02:12 +0800

    证据分解界面部分交互

--- visual-project/src/views/ModelPages/ResolveView.jsx
+++ visual-project/src/views/ModelPages/ResolveView.jsx
@@ -9,6 +9,8 @@ import TextField from "@material-ui/core/TextField";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
+
+
 // import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
@@ -34,6 +36,8 @@ import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 import Add from "@material-ui/icons/Add";
 import CustomTabs from "../../components/CustomTabs/CustomTabs";
 // import { CardTitle } from "assets/jss/material-kit-react.js";
+
+import * as Util from "../../util/Util";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -192,6 +196,8 @@ function EvidenceTabContent(props) {
   );
 }
 
+
+
 // 函数式写法，无class
 export default function ResolveView() {
   // 原告证据
@@ -199,13 +205,108 @@ export default function ResolveView() {
   // 被告证据
   const [defendantDoc, setDefendantDoc] = React.useState([]);
 
+  const [caseDetail, setCaseDetail] = React.useState({
+      caseNum : '（2015）浦刑初字第2978号',
+      caseName : '上海市浦东新区人民检察院以沪浦检刑诉（2015）2315号起诉书指控被告人王某某犯危险驾驶罪案',
+      userName : '孙志刚'
+  })
+
   React.useEffect(() => {
     document.title = "证据分解";
     setProsecutorDoc(JSON.parse(DocumentData.documents));
     setDefendantDoc(JSON.parse(DocumentData.documents));
+
+    //初始化得到案件的基本信息（案号，案件名称，承办人）
+    const url = '/evidence/getCaseDeatal'
+    let param = JSON.stringify({
+        username : sessionStorage.getItem("username"),
+        caseID   : 1
+    });
+
+    const succ = (response) => {
+        console.log(response)
+    };
+
+    const err = () => {
+        console.log('证据分解界面数据初始化异常')
+    };
+
+    Util.asyncHttpGet(url, param ,succ , err );
+
   }, []);
 
+
+
   const [editing, setEditing] = React.useState(-1);
+
+
+  //原告证据
+  const [prosecutorEvidence,setProsecutorEvidence] = React.useState('');
+
+  //被告证据
+  const [defendantEvidence,setDefendantEvidence] = React.useState('');
+
+  //分解原告证据
+  const handleProsecutorResolve = ()=> {
+      alert(prosecutorEvidence)
+      const url = '/evidence/document'
+      let param = JSON.stringify({
+          caseId : sessionStorage.getItem('caseId'),
+          type : 0,
+          text : prosecutorEvidence
+      });
+
+      const succ = (response) => {
+        //保存documentId
+        var documentId = response[0].documentId;
+        sessionStorage.setItem('documentId', documentId);
+        //设置原告分解完证据列表
+        setProsecutorDoc(response);
+      };
+
+      const err = () => {
+          console.log('证据分解界面原告分解证据异常')
+      };
+
+      Util.asyncHttpGet(url, param ,succ , err );
+  }
+
+    //分解被告证据
+    const handleDefendantResolve = ()=> {
+        alert(defendantEvidence)
+        const url = '/evidence/document'
+        let param = JSON.stringify({
+            caseId : sessionStorage.getItem('caseId'),
+            type : 1,
+            text : defendantEvidence
+        });
+
+        const succ = (response) => {
+            //保存documentId
+            var documentId = response[0].documentId;
+            sessionStorage.setItem('documentId', documentId);
+            //设置被告分解完证据列表
+            setDefendantDoc(response)
+        };
+
+        const err = () => {
+            console.log('证据分解界面被告分解证据异常')
+        };
+
+        Util.asyncHttpGet(url, param ,succ , err );
+    }
+
+
+
+  const handleEvidenceChange = field => event =>{
+    if(field === 'prosecutor'){
+      setProsecutorEvidence(event.target.value)
+    }
+    else {
+      setDefendantEvidence(event.target.value)
+    }
+  }
+
 
   const handleClickEdit = (id) => {
     console.log(id);
@@ -227,7 +328,23 @@ export default function ResolveView() {
     }
   };
 
-  const classes = useStyles();
+
+
+
+  //新增原告证据
+  const addNewProsecutorEvidence = () =>{
+    // let oldProsecutorDoc = prosecutorDoc;
+    // oldProsecutorDoc.push({
+    //     documentId : sessionStorage.getItem('documentId'),
+    //     type : '0',
+    //     body : '新证据内容',
+    //     agree : false
+    // });
+    // setProsecutorDoc(oldProsecutorDoc);
+  };
+
+
+    const classes = useStyles();
 
   return (
     <div className={classes.root}>
@@ -240,19 +357,19 @@ export default function ResolveView() {
                 <TableHead>
                   <TableRow>
                     <TableCell>案件号</TableCell>
-                    <TableCell>（2015）浦刑初字第2978号</TableCell>
+                    <TableCell>{caseDetail.caseNum}</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   <TableRow>
                     <TableCell>案件名称</TableCell>
                     <TableCell>
-                      上海市浦东新区人民检察院以沪浦检刑诉（2015）2315号起诉书指控被告人王某某犯危险驾驶罪案
+                        {caseDetail.caseName}
                     </TableCell>
                   </TableRow>
                   <TableRow>
                     <TableCell>承办人</TableCell>
-                    <TableCell>孙志刚</TableCell>
+                    <TableCell>{caseDetail.userName}</TableCell>
                   </TableRow>
                 </TableBody>
               </Table>
@@ -270,12 +387,14 @@ export default function ResolveView() {
                 rows={7}
                 variant="outlined"
                 className={classes.textFieldBlock}
+                onChange={handleEvidenceChange('prosecutor')}
               />
               <CustomButton
                 type="button"
                 color="success"
                 style={{ marginRight: 10 }}
                 round
+                onClick={handleProsecutorResolve}
               >
                 <CropIcon />
                 分解证据
@@ -298,12 +417,14 @@ export default function ResolveView() {
                 rows={7}
                 variant="outlined"
                 className={classes.textFieldBlock}
+                onChange={handleEvidenceChange('defendant')}
               />
               <CustomButton
                 type="button"
                 color="success"
                 style={{ marginRight: 10 }}
                 round
+                onClick={handleDefendantResolve}
               >
                 <CropIcon />
                 分解证据
@@ -339,9 +460,12 @@ export default function ResolveView() {
                       ))}
                     </List>
                     <Paper elevation={0} className={classes.addButton}>
-                      <CustomButton color="success">
+                      <CustomButton
+                          color="success"
+                          onClick={addNewProsecutorEvidence}
+                      >
                         <Add />
-                        添加
+                        添加原告证据
                       </CustomButton>
                     </Paper>
                   </div>
@@ -366,9 +490,12 @@ export default function ResolveView() {
                       ))}
                     </List>
                     <Paper elevation={0} className={classes.addButton}>
-                      <CustomButton color="success">
+                      <CustomButton
+                          color="success"
+
+                      >
                         <Add />
-                        添加
+                        添加被告证据
                       </CustomButton>
                     </Paper>
                   </div>
